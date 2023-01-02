c1ab360abd74cf1ff492039ccf9e85b39784f308
tech <kachmar.dmitriu@ymail.com>
2023-01-02 15:18:12 +0200

    удалено FavStation.kt

--- app/src/main/java/com/vulture/superradio/ui/models/FavStation.kt
+++
@@ -1,9 +0,0 @@
-package com.vulture.superradio.ui.models
-
-data class FavStation(
-    val name: String,
-    val imageUrl: String,
-    val audioSourceUrl: String,
-    val genre: String,
-    val isFavourite: Boolean
-)
